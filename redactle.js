5753fb9ff034820a600e001855df74491a961f0f
skypotatoes <ronan.dodds@gmail.com>
2022-10-18 13:58:17 +0100

    some stuff

--- redactle.js
+++ redactle.js
@@ -1,25 +1,28 @@
-const article = document.querySelector(".article-div");
-const input = document.querySelector(".input-div");
+const article = document.querySelector('.article-div')
+const input = document.querySelector('.input-div')
+const guesses = document.querySelector('.guesses-div')
 
-const sentence = "This is the first sentence is that you need is to guess?";
+const sentence = 'This is the first sentence is that you need is to guess?'
 
-const regex = /[^a-z]/gi;
+const regex = /[^a-z]/gi
 
-let redactedSentence = "";
+let redactedSentence = ''
+const guessesArray = []
+const justGuessesArray = []
 
 for (let i = 0; i < sentence.length; i++) {
-  const matched = sentence[i].match(regex);
+  const matched = sentence[i].match(regex)
   //   console.log(matched);
   if (matched) {
-    redactedSentence += matched;
+    redactedSentence += matched
   } else {
-    redactedSentence += "X";
+    redactedSentence += 'X'
   }
 }
 
-article.append(redactedSentence);
+article.append(redactedSentence)
 
-const freeLibrary = ["is", "the", "a", "an"];
+const freeLibrary = ['is', 'the', 'a', 'an']
 
 const form = `
 
@@ -27,27 +30,59 @@ const form = `
   Guess <input type="text" name="fname" id="box"><br>
   <input type="button" onclick="guessSubmit()" value="Submit">
 </form>
-`;
+`
 
-let guess = "";
+let guess = ''
 
 const guessSubmit = () => {
-  guess = document.getElementById("box").value;
+  guess = document.getElementById('box').value
 
-  checkGuess();
-};
+  checkGuess()
+}
 
 const checkGuess = () => {
-  const guessRegex = new RegExp(`${guess}`, "gi");
-  // UP TO HERE - HAVE CAPTURED INSTANCES OF GUESSED WORD
-  console.log(guessRegex);
-  console.log(sentence);
-  console.log(sentence.match(guessRegex));
-};
+  document.getElementById('frm1').reset()
 
-input.insertAdjacentHTML("beforeend", form);
+  if (justGuessesArray.includes(guess)) {
+  } else {
+    const guessRegex = new RegExp(`\\b(${guess})\\b`, 'gi')
+    // UP TO HERE - HAVE CAPTURED INSTANCES OF GUESSED WORD
+    // console.log(guessRegex)
+    // console.log(sentence)
+    console.log(sentence.match(guessRegex))
+    const matchArray = [...sentence.matchAll(guessRegex)]
+    console.log(matchArray)
+    const indices = []
+    guessesArray.push([
+      `<div class = "guess">${guess}`,
+      `${matchArray.length}</div>`,
+    ])
+    justGuessesArray.push(guess)
 
-console.log(redactedSentence);
+    matchArray.forEach((element) => {
+      indices.push(element.index)
+    })
+    console.log(indices)
+
+    const redactedArray = redactedSentence.split('')
+    console.log(redactedArray)
+    indices.forEach((element) => {
+      for (let i = 0; i < guess.length; i++) {
+        console.log(guess[i])
+        redactedArray.splice(element + i, 1, guess[i])
+        redactedSentence = redactedArray.join('')
+      }
+    })
+    console.log(redactedSentence)
+
+    $('.article-div').html(redactedSentence)
+    $('.guesses-div').html(`<div class="guesslist">${guessesArray}</div>`)
+  }
+}
+
+input.insertAdjacentHTML('beforeend', form)
+
+console.log(redactedSentence)
 
 // const array = sentence.split(" ");
 
